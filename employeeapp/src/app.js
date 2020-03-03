4baae1daadc6931ccc89557c0f832ca58eba1265
Brittany <britloy91@gmail.com>
2020-03-03 06:58:46 -0500

    style sheets/error still occurring

---
+++ employeeapp/src/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/header";
+
+import "./app.css";
+
+function app() {
+  return (
+    <div className="app">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default app;
